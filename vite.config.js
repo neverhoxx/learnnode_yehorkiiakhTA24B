8b26cf7fd714bb95c027aa8c965c74ad58769696
yehor <yehor.kiiakh@gmail.com>
2026-05-25 17:02:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.VITE_BASE ?? '/',
   plugins: [vue()],
-});
+  base: '/learnnode_yehorkiiakhTA24B/'
+})
